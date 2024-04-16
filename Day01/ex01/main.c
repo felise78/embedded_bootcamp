095633711b2a1e482d25640dc150d459d1fc17df
Felise <felise78@gmail.com>
2024-04-16 23:09:24 +0200

    need to test the ex03 tomorrow and verify the Makefile

--- Day01/ex01/main.c
+++ Day01/ex01/main.c
@@ -7,14 +7,23 @@ int main()
 
 	DDRB |= (1 << DDB1);
 
-	TCCR1A |= (1 << COM1A1) | (1 << WGM11);
-	TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS12) | (1 << CS10);
+	// je set le mode 14 : 
+	TCCR1A |= (1 << WGM11);
+	TCCR1B |= (1 << WGM13);
+	TCCR1B |= (1 << WGM12);
+
+	// pre-division a 1024
+	TCCR1B |= (1 << CS12);
+	TCCR1B |= (1 << CS10);
+
+	// comportement de la broche, ici mode non-inverse
+	// OC1A = 0 lors de la detection d'egalite
+	// OC1A = 1 lorsque le compteur passe a 0 (BOTTOM)
+	TCCR1A |= (1 << COM1A1);
 
 	ICR1 = F_CPU / 1024;
 	OCR1A = ICR1 / 2;
 
 	while (1)
-	{
 		;
-	}
 }
--- Day01/ex02/main.c
+++ Day01/ex02/main.c
@@ -7,20 +7,22 @@ int main()
 
 	DDRB |= (1 << DDB1);
 
-	TCCR1A |= (1 << COM1A1) | (1 << WGM11);
-	TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS12) | (1 << CS10);
+	// je set le mode 14 : 
+	TCCR1A |= (1 << WGM11);
+	TCCR1B |= (1 << WGM13);
+	TCCR1B |= (1 << WGM12);
 
-    // Calculating the values for 1Hz frequency and 10% duty cycle
-    // Frequency = F_CPU / (N * (1 + TOP)), where N is the prescaler and TOP is ICR1
-    // 1Hz = F_CPU / (1024 * (1 + ICR1))
-    // ICR1 = F_CPU / (1Hz * 1024) - 1
-    // Duty cycle = OCR1A / ICR1
-    // 10% = OCR1A / ICR1
-    // OCR1A = 0.1 * ICR1
+	// pre-division a 1024
+	TCCR1B |= (1 << CS12);
+	TCCR1B |= (1 << CS10);
+
+	// comportement de la broche, ici mode non-inverse
+	// OC1A = 0 lors de la detection d'egalite
+	// OC1A = 1 lorsque le compteur passe a 0 (BOTTOM)
+	TCCR1A |= (1 << COM1A1);
 
 	ICR1 = F_CPU / 1024;
 	OCR1A = ICR1 / 10;
-
 
 	while (1)
 		;
